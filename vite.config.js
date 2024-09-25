a28887d59121d2af96bec5ab6aa07831c68ccda4
layelhajji <layelhajji3@gmail.com>
2024-09-25 10:58:54 +0100

    changing vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio/', // Ce chemin doit correspondre au nom du dépôt
   plugins: [react()],
-  
-})
+});
+
 
